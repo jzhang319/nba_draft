309ac843abdfcedc6fbe3330669e9696efa771cb
jzhang319 <jzhang319@gmail.com>
2024-02-20 01:11:43 -0500

    updated user model

--- app/models/user.py
+++ app/models/user.py
@@ -20,7 +20,7 @@ class User(db.Model, UserMixin):
     updated_at = db.Column(db.DateTime(timezone=True),
                            server_default=func.now())
 
-    leagues = db.relationship("League", back_populates='users', cascade="all, delete-orphan")
+    leagues = db.relationship("League", back_populates='user', cascade="all, delete-orphan")
 
     @property
     def password(self):
